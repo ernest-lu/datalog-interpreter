135953389b4e6e108fa0eef082b711c3ad06c37e
ernest-lu <53326307+ernest-lu@users.noreply.github.com>
2024-10-31 16:06:40 -0700

    implement semi-naive

--- IDEAS.md
+++ IDEAS.md
@@ -1,2 +1,22 @@
 - Datalog programs have been applied to data integration, networking, and program analysis
+- Implement semi-naive evaluation
+    - Ask for explanation on this. (Expanding the frontier)?
+- Cross-language function calls on datalog
+- Datalog + static analysis
+    - Find security vulnerabilities in code
+    - Run static analysis on bril programs
 
+- Why embed languages
+ - Programmers like to use libraries to express their language
+ - SQLLite is an embedded database engine
+ 
+ - Lambdas
+ - Recursive closures
+ - Datalog static analysis of rust code
+ - People like using datalog to figure out mutually recursive information about their programs
+ - Datalog for debugging
+ - Sanitizer implemented
+ - Datalog for idea generation
+ - Datalog for input generation
+    - Variables and bounds
+- Datalog to search state spaces?
--- src/implem.rs
+++ src/implem.rs
@@ -51,6 +51,13 @@ pub fn run_datalog(program: Program, input: Vec<Fact>) -> Result<Vec<Fact>, Stri
 
     let mut facts_hashset: HashSet<Fact> = HashSet::from_iter(input.iter().cloned());
 
+    // (R X P)
+    // ((r + R) X (p + P))
+    // rp, Rp, RP, rP
+
+    let mut previous_frontier: HashSet<Fact> = facts_hashset.clone();
+    let mut old_frontier: HashSet<Fact> = HashSet::new();
+
     loop {
         // loop until convergence
         let mut new_facts = vec![];
@@ -60,52 +67,70 @@ pub fn run_datalog(program: Program, input: Vec<Fact>) -> Result<Vec<Fact>, Stri
 
             // iterate over all permutations of the body facts
             let facts_vec = facts_hashset.clone().into_iter().collect::<Vec<_>>();
-            for subset in facts_vec.into_iter().permutations(num_body_facts) {
-                // Process each subset of body facts
 
-                // Check if each fact name matches the corresponding body declaration
-                let matches = subset.iter().zip(&rule.body).all(|(fact, decl)| {
-                    fact.name == decl.name && fact.params.len() == decl.params.len()
-                });
+            for size_new_frontier in 1..=num_body_facts {
+                // iterate at least one new fact from the previous frontier for semi-naive evaluation
+                let size_old_frontier = num_body_facts - size_new_frontier;
+                for subset_new in previous_frontier
+                    .clone()
+                    .into_iter()
+                    .combinations(size_new_frontier)
+                {
+                    for subset_old in old_frontier
+                        .clone()
+                        .into_iter()
+                        .combinations(size_old_frontier)
+                    {
+                        let combined_subset = [subset_new.clone(), subset_old.clone()].concat();
+                        for subset in combined_subset.into_iter().permutations(num_body_facts) {
+                            // Check if each fact name matches the corresponding body declaration
+                            let matches = subset.iter().zip(&rule.body).all(|(fact, decl)| {
+                                fact.name == decl.name && fact.params.len() == decl.params.len()
+                            });
 
-                if matches {
-                    let parameter_locations = get_parameter_locations(&subset);
+                            if matches {
+                                let parameter_locations = get_parameter_locations(&subset);
 
-                    let mut parameter_name_mappings = HashMap::new();
-                    let mut okay = true;
-                    for (i, decl) in rule.body.iter().enumerate() {
-                        for (j, param) in decl.params.iter().enumerate() {
-                            if parameter_name_mappings.contains_key(param) {
-                                continue;
+                                let mut parameter_name_mappings = HashMap::new();
+                                let mut okay = true;
+                                for (i, decl) in rule.body.iter().enumerate() {
+                                    for (j, param) in decl.params.iter().enumerate() {
+                                        if parameter_name_mappings.contains_key(param) {
+                                            continue;
+                                        }
+                                        let actual_parameter_name = &subset[i].params()[j];
+                                        let expected_parameter_hashset =
+                                            expected_parameter_locations.get(param).unwrap();
+                                        let actual_parameter_hashset =
+                                            parameter_locations.get(actual_parameter_name).unwrap();
+                                        if expected_parameter_hashset != actual_parameter_hashset {
+                                            okay = false;
+                                            break;
+                                        }
+                                        parameter_name_mappings
+                                            .insert(param.clone(), actual_parameter_name.clone());
+                                    }
+                                    if !okay {
+                                        break;
+                                    }
+                                }
+                                if okay {
+                                    let new_fact = Fact {
+                                        name: rule.head.name.clone(),
+                                        params: rule
+                                            .head
+                                            .params
+                                            .iter()
+                                            .map(|p| {
+                                                parameter_name_mappings.get(p).unwrap().clone()
+                                            })
+                                            .collect(),
+                                    };
+                                    if !facts_hashset.contains(&new_fact) {
+                                        new_facts.push(new_fact);
+                                    }
+                                }
                             }
-                            let actual_parameter_name = &subset[i].params()[j];
-                            let expected_parameter_hashset =
-                                expected_parameter_locations.get(param).unwrap();
-                            let actual_parameter_hashset =
-                                parameter_locations.get(actual_parameter_name).unwrap();
-                            if expected_parameter_hashset != actual_parameter_hashset {
-                                okay = false;
-                                break;
-                            }
-                            parameter_name_mappings
-                                .insert(param.clone(), actual_parameter_name.clone());
-                        }
-                        if !okay {
-                            break;
-                        }
-                    }
-                    if okay {
-                        let new_fact = Fact {
-                            name: rule.head.name.clone(),
-                            params: rule
-                                .head
-                                .params
-                                .iter()
-                                .map(|p| parameter_name_mappings.get(p).unwrap().clone())
-                                .collect(),
-                        };
-                        if !facts_hashset.contains(&new_fact) {
-                            new_facts.push(new_fact);
                         }
                     }
                 }
@@ -114,6 +139,9 @@ pub fn run_datalog(program: Program, input: Vec<Fact>) -> Result<Vec<Fact>, Stri
         if new_facts.is_empty() {
             break;
         }
+        // extend the old frontier with the previous frontier
+        old_frontier.extend(previous_frontier.clone());
+        previous_frontier = HashSet::from_iter(new_facts.iter().cloned());
         facts_hashset.extend(new_facts.iter().cloned());
     }
 
