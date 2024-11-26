3729f4d03d776ea1d58b4756d86760e23a2b486c
ernest-lu <53326307+ernest-lu@users.noreply.github.com>
2024-11-26 00:49:46 -0600

    rm svelte

--- web-demo-svelte-backup/web-demo/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import wasmPack from 'vite-plugin-wasm-pack';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte(), wasmPack('./datalog_wasm')],
-})
